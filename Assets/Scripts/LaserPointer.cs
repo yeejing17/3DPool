50615def92e7421d258a498ed537b468a3791541
Andy Jiahui Z <zhuj0017@e.ntu.edu.sg>
2018-03-01 17:17:12 +0800

    cue stick holdable

--- Assets/Scripts/LaserPointer.cs
+++ Assets/Scripts/LaserPointer.cs
@@ -109,7 +109,7 @@ public class LaserPointer : MonoBehaviour
 		shouldTeleport = false; // Teleport in progress, no need to do it again until the next touchpad release
 		reticle.SetActive(false); // Hide reticle
 		Vector3 difference = cameraRigTransform.position - headTransform.position; // Calculate the difference between the center of the virtual room & the player's head
-		difference.y = 53; // Don't change the final position's y position, it should always be equal to that of the hit point
+		difference.y = 0; // Don't change the final position's y position, it should always be equal to that of the hit point
 
 		cameraRigTransform.position = hitPoint + difference; // Change the camera rig position to where the the teleport reticle was. Also add the difference so the new virtual room position is relative to the player position, allowing the player's new position to be exactly where they pointed. (see illustration)
 	}
